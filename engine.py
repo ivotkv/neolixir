f6c996b7e7ec163ecc75dfc691d0cb53cc7052d0
Ivaylo Tzvetkov <ivotkv@gmail.com>
2013-01-21 16:18:53 -0500

    workin' on it

--- engine.py
+++ engine.py
@@ -1,11 +1,26 @@
+import threading
 from py2neo import neo4j, cypher
-from utils import ThreadLocalRegistry as TLR
+
+engine = None
+
+def get_engine():
+    if instance is None:
+        raise ValueError("Neo4j engine not initialized!")
+    return engine.instance
 
 class Engine(object):
 
     def __init__(self, url='http://localhost:7474/db/data/'):
+        self.__threadlocal__ = threading.local()
         self.url = url
-        self.threadsafe = TLR(url, instance=lambda url: neo4j.GraphDatabaseService(url))
+
+    @property
+    def instance(self):
+        try:
+            return self.__threadlocal__.instance
+        except AttributeError:
+            self.__threadlocal__.instance = neo4j.GraphDatabaseService(self.url)
+            return self.__threadlocal__.instance
 
     def execute(self, *args, **kwargs):
-        return cypher.execute(self.threadsafe.instance, *args, **kwargs)
+        return cypher.execute(self.instance, *args, **kwargs)
--- node.py
+++ node.py
@@ -1,15 +1,25 @@
-class NodeMeta(type):
-    pass
+from py2neo.neo4j import Node
+from engine import get_engine
 
-class NodeBase(object):
+class NodeEntityMeta(type):
+
+    def __new__(cls, name, bases, dict_):
+        attrs = ((name, value) for name, value in dct.items() if not name.startswith('__'))
+        uppercase_attr = dict((name.upper(), value) for name, value in attrs)
+
+        return super(NodeEntityMeta, cls).__new__(cls, name, bases, dict_)
+
+class NodeEntity(Node):
+
+    __metaclass__ = NodeEntityMeta
+
+    def __init__(self, **kwargs):
+        pass
 
     @classmethod
-    def query(cls, *args, **kwargs):
-        return None
+    def get_by(cls, **kwargs):
+        e = get_engine()
+        if 'id' in kwargs:
+            return e.get_node(kwargs['id'])
+        else:
 
-    @classmethod
-    def get_by(cls, *args, **kwargs):
-        return None
-
-class Node(NodeBase):
-    __metaclass__ = NodeMeta
