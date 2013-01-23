e5a9db516afc277b653adcbc2907fb4be4149bff
Ivaylo Tzvetkov <ivotkv@gmail.com>
2013-01-23 17:22:07 -0500

    makes more sense now

--- index.py
+++ index.py
@@ -8,46 +8,31 @@ from neolixir import meta
 class Index(object):
 
     _type = Entity
-    _defaultkey = '__default_key__'
 
     def __init__(self, type, name, config=None):
         self._index = meta.engine.get_or_create_index(type, name, config)
 
-    def __getitem__(self, key):
-        try:
-            return self.get(self._defaultkey, key)[0]
-        except IndexError:
-            raise KeyError(key)
+    @property
+    def name(self):
+        return self._index.name
 
-    def __setitem__(self, key, entity):
-        if not self.set(self._defaultkey, key, entity):
-            raise OperationalError("failed to set: '{0}'".format(key))
-
-    def __delitem__(self, key):
-        self.remove(self._defaultkey, key)
-
-    def add(self, key, value, entity, unique=False):
-        func = self._index.add_if_none if unique else self._index.add
+    def add(self, key, value, entity, if_none=False):
+        func = self._index.add_if_none if if_none else self._index.add
         return func(key, value, entity._entity) is not None
 
-    def remove(self, key=None, value=None, entity=None):
-        self._index.remove(key, value, entity._entity if entity else None)
-
-    def get(self, key, value=None):
-        if value is not None:
+    def get(self, key, value=None, abstract=None):
+        if value is None:
+            return map(self._type, self._index.query("{0}:*".format(key)))
+        elif abstract is None:
             return map(self._type, self._index.get(key, value))
         else:
-            return map(self._type, self._index.query("{0}:*".format(key)))
-
-    def set(self, key, value, entity):
-        if not self.add(key, value, entity, unique=True):
-            # NOTE: will run if the key/value is being set to the same node too
-            self.remove(key, value)
-            return self.add(key, value, entity, unique=True)
-        return True
+            return self._type(self._index.get_or_create(key, value, abstract))
 
     def query(self, query):
         return map(self._type, self._index.query(query))
+
+    def remove(self, key=None, value=None, entity=None):
+        self._index.remove(key, value, entity._entity if entity else None)
 
 class NodeIndex(Index):
 
