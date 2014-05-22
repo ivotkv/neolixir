6e156df0fc93ec5112e872933cb996f6bf1de516
Ivo Tzvetkov <ivotkv@gmail.com>
2014-05-22 16:31:20 -0400

    query get_by tests

---
+++ tests/test_query.py
@@ -0,0 +1,18 @@
+from common import *
+from random import randint
+
+def test_get_by(m):
+    v1 = randint(0, 2**30)
+    n1 = TNode(integer=v1)
+    m.session.commit()
+    assert Node.get_by(integer=v1) is n1
+    assert TNode.get_by(integer=v1) is n1
+    with raises(NoResultFound):
+        SubTNode.get_by(integer=v1)
+    m.session.clear()
+    n1 = Node.get_by(integer=v1)
+    assert isinstance(n1, TNode)
+    assert n1.integer == v1
+    assert TNode.get_by(integer=v1) is n1
+    with raises(NoResultFound):
+        SubTNode.get_by(integer=v1)
