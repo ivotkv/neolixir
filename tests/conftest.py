d2d2e6ad0218f2ee8113681ce0b79a0fc7abd7e0
Ivo Tzvetkov <ivotkv@gmail.com>
2014-05-29 16:20:39 -0400

    allowing NEO4J_TEST_SERVER variable to be specified

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+import os
 import pytest
 
 @pytest.fixture(scope='session')
 def metadata():
     from neolixir import metadata
+    if 'NEO4J_TEST_SERVER' in os.environ:
+        metadata.engine = 'http://{0}/db/data/'.format(os.environ['NEO4J_TEST_SERVER'])
     metadata.engine.clear()
     import models
     metadata.init()
