b7b5e77eb43603f38027eb6ca73fa5f96fc14b9a
Ivo Tzvetkov <ivotkv@gmail.com>
2014-05-29 14:38:25 -0400

    more index tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -18,3 +18,21 @@ def test_node_index(m):
     i.clear()
     res = i.get('key', 'value')
     assert len(res) == 0
+    
+    n1 = TNode()
+    res = i.get('key', 'value', n1)
+    assert res is n1
+
+    res = i.get('key', 'value', n1)
+    assert res is n1
+
+    res = i.get('key', 'value')
+    assert len(res) == 1
+    assert res[0] is n1
+
+    n1_id = n1.id
+    m.session.clear()
+    res = i.get('key', 'value')
+    assert len(res) == 1
+    assert isinstance(res[0], TNode)
+    assert res[0].id == n1_id
