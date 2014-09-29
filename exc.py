bffffbbea5f42b557ed1faa79cdfae6ece00594d
Ivo Tzvetkov <ivotkv@gmail.com>
2014-09-29 12:16:26 -0400

    better handling and logging of commit errors

--- exc.py
+++ exc.py
@@ -3,13 +3,25 @@
 from py2neo.rest import BadRequest, ResourceNotFound, ResourceConflict, SocketError
 from py2neo.cypher import CypherError
 
-__all__ = ['NeolixirError', 'QueryError', 'NoResultFound',
-           'MultipleResultsFound', 'CypherError','BadRequest',
-           'ResourceNotFound', 'ResourceConflict', 'SocketError']
+__all__ = ['NeolixirError', 'CommitError',
+           'QueryError', 'NoResultFound', 'MultipleResultsFound',
+           'CypherError','BadRequest', 'ResourceNotFound',
+           'ResourceConflict', 'SocketError']
 
 class NeolixirError(Exception):
     """Base class for all Neolixir exceptions."""
     pass
+
+class CommitError(NeolixirError):
+    """Triggered when an error occurred during session commit."""
+
+    def __init__(self, e, saved, pending):
+        msg = u'{0}: {1}'.format(e.__class__.__name__, e)
+        for request, response in saved:
+            msg += u'\nSAVED: {0} {1} {2} -> {3}'.format(request.method, request.uri, request.body, response)
+        for request in pending:
+            msg += u'\nPENDING: {0} {1} {2}'.format(request.method, request.uri, request.body)
+        super(CommitError, self).__init__(msg)
 
 class QueryError(NeolixirError):
     """Base class for all Neolixir query exceptions."""
