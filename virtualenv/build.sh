50b8f6a5c96993137d7066b252ef59b2dda5f9a6
Ivo Tzvetkov <ivotkv@gmail.com>
2014-11-07 12:09:10 -0500

    improvements to build script

--- virtualenv/build.sh
+++ virtualenv/build.sh
@@ -14,6 +14,7 @@ if [ "$1" = "all" ] || [ "$1" = "py2neo14" ]; then
     rm -rf py2neo14
     virtualenv py2neo14
     source py2neo14/bin/activate
+    pip install ipython
     pip install "py2neo==1.4.6"
     pip install pytest
     ln -s ../../../../../neolixir py2neo14/lib/python*/site-packages/
@@ -23,6 +24,7 @@ if [ "$1" = "all" ] || [ "$1" = "py2neo16" ]; then
     rm -rf py2neo16
     virtualenv py2neo16
     source py2neo16/bin/activate
+    pip install ipython
     pip install "py2neo>=1.6,<1.7"
     pip install pytest
     ln -s ../../../../../neolixir py2neo16/lib/python*/site-packages/
@@ -30,8 +32,9 @@ fi
 
 if [ "$1" = "all" ] || [ "$1" = "py2neo2beta" ]; then
     rm -rf py2neo2beta
-    virtualenv py2neo2beta
+    virtualenv-2.7 py2neo2beta
     source py2neo2beta/bin/activate
+    pip install ipython
     pip install "git+git://github.com/nigelsmall/py2neo.git@beta/2.0#egg=py2neo"
     pip install pytest
     ln -s ../../../../../neolixir py2neo2beta/lib/python*/site-packages/
