9dce486ee7ec6fb5733f9428e017528f5360a2b9
Ivo Tzvetkov <ivotkv@gmail.com>
2014-11-25 14:22:45 -0500

    cleaner handling of threading for helper tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,22 +1,18 @@
-from Queue import Queue
-from threading import Thread
 from neolixir.relmap import RelView
+from utils import *
 
 def create_out_of_session(m, entity):
 
     assert entity.is_phantom()
     entity.expunge()
 
-    def run(q, m, entity):
+    @checked_thread
+    def run(m, entity):
         m.session.add(entity)
         m.session.commit()
-        q.put(entity.id)
+        return entity.id
 
-    q = Queue()
-    t = Thread(target=run, args=(q, m, entity))
-    t.start()
-    t.join()
-    return q.get() if not q.empty() else None
+    return run(m, entity)
 
 def update_out_of_session(m, entity, properties=None):
 
@@ -24,33 +20,27 @@ def update_out_of_session(m, entity, properties=None):
     if properties == None:
         properties = entity.properties
 
-    def run(q, m, entity, properties):
+    @checked_thread
+    def run(m, entity, properties):
         entity = entity.__class__(entity.id)
         entity.properties.update(properties)
         m.session.commit()
-        q.put(m.session.dirty == 0)
+        return m.session.dirty == 0
 
-    q = Queue()
-    t = Thread(target=run, args=(q, m, entity, properties))
-    t.start()
-    t.join()
-    return q.get() if not q.empty() else None
+    return run(m, entity, properties)
 
 def delete_out_of_session(m, entity):
 
     assert not entity.is_phantom()
 
-    def run(q, m, entity):
+    @checked_thread
+    def run(m, entity):
         entity = entity.__class__(entity.id)
         entity.delete()
         m.session.commit()
-        q.put(m.session.count == 0)
+        return entity not in m.session
 
-    q = Queue()
-    t = Thread(target=run, args=(q, m, entity))
-    t.start()
-    t.join()
-    return q.get() if not q.empty() else None
+    return run(m, entity)
 
 def append_out_of_session(m, n1, relname, n2):
 
@@ -59,18 +49,15 @@ def append_out_of_session(m, n1, relname, n2):
     assert isinstance(getattr(n1, relname, None), RelView)
     assert not n2.is_phantom()
 
-    def run(q, m, n1, relname, n2):
+    @checked_thread
+    def run(m, n1, relname, n2):
         n1 = n1.__class__(n1.id)
         n2 = n2.__class__(n2.id)
         rel = getattr(n1, relname).append(n2)
         m.session.commit()
-        q.put(rel.id if rel else None)
+        return rel.id if rel else None
 
-    q = Queue()
-    t = Thread(target=run, args=(q, m, n1, relname, n2))
-    t.start()
-    t.join()
-    return q.get() if not q.empty() else None
+    return run(m, n1, relname, n2)
 
 def remove_out_of_session(m, n1, relname, n2):
 
@@ -79,7 +66,8 @@ def remove_out_of_session(m, n1, relname, n2):
     assert isinstance(getattr(n1, relname, None), RelView)
     assert not n2.is_phantom()
 
-    def run(q, m, n1, relname, n2):
+    @checked_thread
+    def run(m, n1, relname, n2):
         n1 = n1.__class__(n1.id)
         n2 = n2.__class__(n2.id)
         relview = getattr(n1, relname)
@@ -87,12 +75,8 @@ def remove_out_of_session(m, n1, relname, n2):
             count = len(relview)
             relview.remove(n2)
             m.session.commit()
-            q.put(len(relview) == count - 1)
+            return len(relview) == count - 1
         else:
-            q.put(False)
+            return False
 
-    q = Queue()
-    t = Thread(target=run, args=(q, m, n1, relname, n2))
-    t.start()
-    t.join()
-    return q.get() if not q.empty() else None
+    return run(m, n1, relname, n2)
--- tests/utils.py
+++ tests/utils.py
@@ -1,2 +1,32 @@
+import sys
+import traceback
+from Queue import Queue
+from threading import Thread
+
 def same_type_and_value(x, y):
     return type(x) is type(y) and x == y
+
+def checked_thread(func):
+
+    def wrapper(*args, **kwargs):
+    
+        def run(q, *args, **kwargs):
+            try:
+                q.put(func(*args, **kwargs))
+            except:
+                q.put(sys.exc_info())
+
+        q = Queue()
+        t = Thread(target=run, args=(q,) + args, kwargs=kwargs)
+        t.start()
+        t.join()
+
+        assert not q.empty(), 'unexpected empty queue in checked_thread()'
+        value = q.get()
+        assert not (isinstance(value, tuple) and 
+                    isinstance(value[0], type) and
+                    issubclass(value[0], Exception)), \
+               ''.join(traceback.format_exception(*value))
+        return value
+
+    return wrapper
