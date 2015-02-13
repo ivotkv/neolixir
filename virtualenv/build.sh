e7cf6cb43023d7bd832ef635682ade38886adc30
Ivo Tzvetkov <ivotkv@gmail.com>
2015-02-13 10:15:58 -0500

    removing unsupported old version from virtualenv build script

--- virtualenv/build.sh
+++ virtualenv/build.sh
@@ -10,16 +10,6 @@ fi
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-if [ "$1" = "all" ] || [ "$1" = "py2neo14" ]; then
-    rm -rf py2neo14
-    virtualenv py2neo14
-    source py2neo14/bin/activate
-    pip install ipython
-    pip install "py2neo==1.4.6"
-    pip install pytest
-    ln -s ../../../../../neolixir py2neo14/lib/python*/site-packages/
-fi
-
 if [ "$1" = "all" ] || [ "$1" = "py2neo20" ]; then
     rm -rf py2neo20
     virtualenv --python=python2.7 py2neo20
