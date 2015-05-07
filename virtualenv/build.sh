16eed8bf39cf13e0ad2ef15b64e050e7c51d0423
Ivo Tzvetkov <ivotkv@gmail.com>
2015-05-07 18:34:00 -0400

    tests pass with py2neo 2.0.6

--- virtualenv/build.sh
+++ virtualenv/build.sh
@@ -10,13 +10,13 @@ fi
 
 cd $(dirname "${BASH_SOURCE[0]}")
 
-if [ "$1" = "all" ] || [ "$1" = "py2neo204" ]; then
-    rm -rf py2neo204
-    virtualenv --python=python2.7 py2neo204
-    source py2neo204/bin/activate
+if [ "$1" = "all" ] || [ "$1" = "py2neo206" ]; then
+    rm -rf py2neo206
+    virtualenv --python=python2.7 py2neo206
+    source py2neo206/bin/activate
     pip install ipython
-    pip install "py2neo==2.0.4"
+    pip install "py2neo==2.0.6"
     pip install simplejson
     pip install pytest
-    ln -s ../../../../../neolixir py2neo204/lib/python*/site-packages/
+    ln -s ../../../../../neolixir py2neo206/lib/python*/site-packages/
 fi
